4139378def41a01e9ce4d26d0acf7f828036e09e
kovalevivan2004 <kovalevivan2004@yandex.ru>
2024-06-07 22:14:33 +0300

    2 version

--- app.py
+++ app.py
@@ -38,45 +38,92 @@ def contract(contract_id):
     pos = get_contract(contract_id)
     return render_template('contract.html', contract=pos)
 
-# Квартиры 
+# Квартиры
+
 
 @app.route('/trainings')
-def training():
+def trainings():
     conn = get_db_connection()
-    pos = conn.execute("""SELECT * FROM contracts, trainings, clients
-    WHERE contracts.training_id = trainings.id_training and contracts.client_id = clients.id_client
-    """).fetchall()
+    pos = conn.execute("""SELECT * FROM trainings""").fetchall()
     conn.close()
-    return render_template('trainings.html', training=pos)
+    return render_template('trainings.html', trainings=pos)
 
 def get_training(item_id):
     conn = get_db_connection()
-    item = conn.execute("""SELECT * FROM contracts, trainings, clients
-    WHERE contracts.training_id = trainings.id_training and contracts.client_id = clients.id_client and trainings.id_training = ?
-                        """, (item_id,)).fetchone()
+    item = conn.execute("""SELECT * FROM trainings WHERE id_training = ?""", (item_id,)).fetchone()
     conn.close()
     if item is None:
         abort(404)
     return item
 
-'''@app.route('/training/<int:training_id>')
+@app.route('/training/<int:training_id>')
 def training(training_id):
     pos = get_training(training_id)
     return render_template('training.html', training=pos)
-'''
-#/contract/<int:contract_id>
+
 
 @app.route('/clients')
 def clients():
-    abort(404)
+    conn = get_db_connection()
+    pos = conn.execute("""SELECT * FROM clients""").fetchall()
+    conn.close()
+    return render_template('clients.html', clients=pos)
+
+def get_client(item_id):
+    conn = get_db_connection()
+    item = conn.execute("""SELECT * FROM clients WHERE id_client = ?""", (item_id,)).fetchone()
+    conn.close()
+    if item is None:
+        abort(404)
+    return item
+
+@app.route('/client/<int:client_id>')
+def client(client_id):
+    pos = get_client(client_id)
+    return render_template('client.html', client=pos)
+
+
 
 @app.route('/reports')
 def reports():
-    abort(404)
+    conn = get_db_connection()
+    pos = conn.execute("""SELECT * FROM reports""").fetchall()
+    conn.close()
+    return render_template('reports.html', reports=pos)
+
+def get_report(item_id):
+    conn = get_db_connection()
+    item = conn.execute("""SELECT * FROM reports WHERE id_report = ?""", (item_id,)).fetchone()
+    conn.close()
+    if item is None:
+        abort(404)
+    return item
+
+@app.route('/report/<int:report_id>')
+def report(report_id):
+    pos = get_report(report_id)
+    return render_template('report.html', report=pos)
+
 
 @app.route('/employees')
 def employees():
-    abort(404)
+    conn = get_db_connection()
+    pos = conn.execute("""SELECT * FROM employees""").fetchall()
+    conn.close()
+    return render_template('employees.html', employees=pos)
+
+def get_employee(item_id):
+    conn = get_db_connection()
+    item = conn.execute("""SELECT * FROM employees WHERE id_employee = ?""", (item_id,)).fetchone()
+    conn.close()
+    if item is None:
+        abort(404)
+    return item
+
+@app.route('/employee/<int:employee_id>')
+def employee(employee_id):
+    pos = get_employee(employee_id)
+    return render_template('employee.html', employee=pos)
 
 @app.errorhandler(404)
 def page_not_found(e):
