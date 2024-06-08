4da237f1599cbef29748d1a2e18f6eb414514ce2
kovalevivan2004 <kovalevivan2004@yandex.ru>
2024-06-08 20:57:48 +0300

    4 version

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
+import os
 import sqlite3
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, request, flash, send_from_directory
 from werkzeug.exceptions import abort
+import docx
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = b'my)secret)key'
+UPLOAD_FOLDER = 'contracts'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 def get_db_connection():
     conn = sqlite3.connect('../pythonProject4/database.db')
@@ -12,7 +18,132 @@ def get_db_connection():
 def index():
     return redirect("/contracts")
 
-# Контракты 
+
+@app.route('/new_contract', methods=('GET', 'POST'))
+def new_contract():
+    """ Страница-добавления нового контракта """
+
+    if request.method == 'POST':
+        # добавление нового контракта в БД псоле заполнения формы
+
+        number = request.form['number']
+        date = request.form['date']
+        price = int(request.form['price'])
+        discount = int(request.form['discount'])
+        finish_price = None
+        deal_status = 0
+        client_id = int(request.form.get('client'))
+        training_id = int(request.form.get('training'))
+        employee_id = int(request.form.get('employee'))
+        if not (client_id > 0 and training_id > 0 and employee_id > 0):
+            flash('Не все поля заполнены')
+        else:
+            if not (number and date and price and discount ):
+                flash('Не все поля заполнены')
+            else:
+                conn = get_db_connection()
+                cursor = conn.cursor()
+                cursor.execute(
+                    "INSERT INTO 'contracts' ('number', 'date', 'price', 'discount', 'deal_status', 'finish_price', 'training_id', 'client_id', 'employee_id')  VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)",
+                    (number, date, price, discount, deal_status, finish_price, training_id, client_id,
+                     employee_id))
+                conn.commit()
+                new_contract_id = cursor.lastrowid
+                print(new_contract_id)
+                conn.close()
+                return redirect(f'/contract/{new_contract_id}')
+
+    # отрисовка формы
+    conn = get_db_connection()
+    pos1 = conn.execute("""SELECT * FROM clients""").fetchall()
+    pos2 = conn.execute("""SELECT * FROM trainings""").fetchall()
+    pos3 = conn.execute("""SELECT * FROM employees""").fetchall()
+    conn.close()
+    return render_template('new_contract.html', clients=pos1, trainings=pos2, employees=pos3)
+
+
+@app.route('/generate_contract', methods=('GET', 'POST'))
+def generate_contract():
+    """ Страница генерации договора """
+
+    # переменные шаблона
+    id = int(request.args.get('id_contract'))
+    conn = get_db_connection()
+    pos = conn.execute("""SELECT * FROM contracts, trainings, clients, employees
+            WHERE contracts.training_id = trainings.id_training and 
+                        contracts.client_id = clients.id_client and 
+                        contracts.employee_id = employees.id_employee and
+                        contracts.id_contract = ?
+                        """, (id,)).fetchone()
+    conn.close()
+    contract_params = {
+        'CLIENT_BIRTHDATE': 'дата рождения клиента',
+        'CLIENT_BIRTHPLACE': 'место рождения клиента',
+        'CLIENT_PASSPORT_DATE': 'дата выдачи паспорта клиента',
+        'CLIENT_PASSPORT_DEPARTMENT': 'подразделение, выдавшее паспорт клиента',
+        'CLIENT_PASSPORT_DEPCODE': 'код подразделения, выдавшего паспорт клиента',
+        'CLIENT_REG_ADDRESS': 'адрес регистрации клиента',
+        'CONTRACT_DURATION': 'длительность контракта',
+        'CONTRACT_PRICE': 'цена абонемента'}
+    contract_params_auto = {
+        'CONTRACT_NUMBER': ['номер договора', pos['number']],
+        'CONTRACT_DATE': ['дата подписания договора', pos['date']],
+        'CLIENT_FULLNAME': ['ФИО клиента', pos['name']],
+        'CLIENT_PASSPORT_NUMBER': ['серия и номер паспорта клиента', pos['passport']],
+        'EMPLOYEE_POSITION': ['должность сотрудника риэлторской компании', pos['position']],
+        'EMPLOYEE_FULLNAME': ['ФИО сотрудника риэлторской компании', pos['name']],}
+
+    if request.method == 'POST':
+        # создание нового документа
+        result_params = request.form.to_dict()
+        create_contract(id, result_params)
+        return redirect(f'/contract/{id}')
+
+    # скачивание файла, если он заполнен
+    filename = f"договор {pos['number']} от {pos['date']}.docx"
+    if os.path.exists(os.path.join('pythonProject4/contracts', filename)):
+        return send_from_directory(app.config['UPLOAD_FOLDER'], filename, as_attachment=True)
+    else:
+        # отрисовка формы заполнения
+        flash('Договор не сформирован, заполните его')
+        return render_template('generate_contract.html',
+                               contract=pos, contract_params=contract_params, auto_params=contract_params_auto)
+
+
+def create_contract(id, contract_params):
+    """ Создание нового документа по шаблону """
+
+    template = os.path.join('pythonProject4/contracts', 'contract_template.docx')
+    result = os.path.join('pythonProject4/contracts',
+                          f"договор {contract_params['CONTRACT_NUMBER']} от {contract_params['CONTRACT_DATE']}.docx")
+
+    template_doc = docx.Document(template)
+    for key, value in contract_params.items():
+        for paragraph in template_doc.paragraphs:
+            replace_text(paragraph, f'=={key}==', value)
+        for table in template_doc.tables:
+            replace_text_in_tables(table, key, value)
+    template_doc.save(result)
+
+
+def replace_text(paragraph, key, value):
+    """ Работа docx - заполнение параграфов """
+
+    if key in paragraph.text:
+        paragraph.text = paragraph.text.replace(key, value)
+
+
+def replace_text_in_tables(table, key, value):
+    """ Работа docx - заполнение таблиц """
+
+    for row in table.rows:
+        for cell in row.cells:
+            if key in cell.text:
+                cell.text = cell.text.replace(key, value)
+
+
+
+# Контракты
 
 @app.route('/contracts')
 def contracts():
@@ -39,7 +170,87 @@ def contract(contract_id):
     return render_template('contract.html', contract=pos)
 
 # Квартиры
+'''
+    if request.method == 'POST':
+        # добавление нового контракта в БД псоле заполнения формы
+        try:
+            number = request.form['number']
+            date = request.form['date']
+            price = int(request.form['price'])
+            discount = int(request.form['discount'])
+            finish_price = price*discount
+            deal_status = 0
+            client_id = int(request.form.get('client'))
+            training_id = int(request.form.get('training'))
+            employee_id = int(request.form.get('employee'))
+        except ValueError:
+            flash('Некорректные значения')
+            client_id = 0
+        if not (client_id > 0 and training_id > 0 and employee_id > 0):
+            flash('Не все поля заполнены')
+        else:
+            if not (number and date and price and discount ):
+                flash('Не все поля заполнены')
+            else:
+                conn = get_db_connection()
+                cursor = conn.cursor()
+                cursor.execute(
+                    "INSERT INTO 'contracts' ('number', 'date', 'price', 'discount', 'deal_status', 'finish_price', 'training_id', 'client_id', 'employee_id')  VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)",
+                    (number, date, price, discount, deal_status, finish_price, training_id, client_id,
+                     employee_id))
+                conn.commit()
+                new_contract_id = cursor.lastrowid
+                print(new_contract_id)
+                conn.close()
+                return redirect(f'/contract/{new_contract_id}')
 
+    # отрисовка формы
+    conn = get_db_connection()
+    pos1 = conn.execute("""SELECT * FROM clients""").fetchall()
+    pos2 = conn.execute("""SELECT * FROM trainings""").fetchall()
+    pos3 = conn.execute("""SELECT * FROM employees""").fetchall()
+    conn.close()
+    return render_template('new_contract.html', clients=pos1, trainings=pos2, employees=pos3)
+'''
+def new_training():
+    """ Страница-добавления новой квартиры """
+
+
+    if request.method == 'POST':
+        # добавление нового контракта в БД псоле заполнения формы
+        try:
+            duration = int(request.form['duration'])
+            description = request.form['description']
+            amount = int(request.form['amount'])
+            client_id = int(request.form.get('client'))
+            employee_id = int(request.form.get('employee'))
+        except ValueError:
+            flash('Некорректные значения')
+            client_id = 0
+        if not (client_id > 0  and employee_id > 0):
+            flash('Не все поля заполнены')
+        else:
+            if not (duration and description and amount ):
+                flash('Не все поля заполнены')
+            else:
+                conn = get_db_connection()
+                cursor = conn.cursor()
+                cursor.execute(
+                    "INSERT INTO 'contracts' ('duration', 'description', 'amount', 'client_id', 'employee_id')  VALUES (?, ?, ?, ?, ?)",
+                    (duration, description, amount, client_id,
+                     employee_id))
+                conn.commit()
+                new_training_id = cursor.lastrowid
+                print(new_training_id)
+                conn.close()
+                return redirect(f'/contract/{new_training_id}')
+
+    # отрисовка формы
+    conn = get_db_connection()
+    pos1 = conn.execute("""SELECT * FROM clients""").fetchall()
+    pos3 = conn.execute("""SELECT * FROM employees""").fetchall()
+    conn.close()
+    return render_template('new_training.html', clients=pos1, employees=pos3)
 
 @app.route('/trainings')
 def trainings():
